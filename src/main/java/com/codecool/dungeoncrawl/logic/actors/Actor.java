57610d5b3e2d3b4278bb542e93e1d9f7af1371c8
FenglerPiotr <piotfeng@gmail.com>
2023-10-11 20:59:32 +0200

    This commit provides movement validation.

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -1,7 +1,10 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
+
+import static com.codecool.dungeoncrawl.logic.CellType.WALL;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
@@ -14,11 +17,12 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
+        if (!nextCell.isOccupied()) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
     }
-
     public int getHealth() {
         return health;
     }
